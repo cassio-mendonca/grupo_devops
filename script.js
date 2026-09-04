219fc84feb1731fa5ffcf0656b7ff9fa68b898e5
pedro-novaes00 <condyy456@gmail.com>
2026-09-04 20:33:39 -0300

    Adicionei a função para o botão do "Ver serviços".
    Após a revisão do cliente, foi feito o pedido para acrescentar a função para o botão.

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const btn = document.querySelectorAll("button");
 const form = document.querySelector("#formContato");
 const resposta = document.querySelector("#resposta");
- 
+const servicos = document.querySelector("#servicos");
+
 btn.forEach(b => {
     b.onmouseover = () => b.style.background = "#57665F";
     b.onmouseout = () => b.style.background = "";
 });
- 
+
+document.querySelector("#botaoDestaque").onclick = () => {
+    servicos.scrollIntoView({ behavior: "smooth" });
+};
+
 form.onsubmit = e => {
     e.preventDefault();
- 
+
     const nome = document.querySelector("#nome").value.trim();
     const mensagem = document.querySelector("#mensagem").value.trim();
- 
+
     if (!nome || !mensagem) {
         resposta.textContent = "Preencha todos os campos!";
         resposta.style.color = "red";
